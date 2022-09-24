e9ba1df746c93247acb7dcf606d7095a4c7edac7
Andrey Potkas <potkasa@gmail.com>
2022-09-24 16:03:09 +0300

    feat: add all nessesary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/pages/list/list.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
